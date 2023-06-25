261f9a3d5e40d0715a13497a338d70ac46faa428
SoloAbyss <maximusabrahamse@gmail.com>
2023-06-26 10:31:00 +1200

    idk yet

--- quiz_test.py
+++ quiz_test.py
@@ -1,4 +1,6 @@
 
+"""This is a Te Reo Maori Quiz."""
+
 import random
 import time
 import os
@@ -60,7 +62,8 @@ questions_veggies = [
     },
     {
         "question": "What is the Maori word for CAULIFLOWER?",
-        "answers": ["Kareparāoa", "Whakanuihuka", "Pōhakahukarere", "Kōhurere"],
+        "answers": ["Kareparāoa", "Whakanuihuka",
+                    "Pōhakahukarere", "Kōhurere"],
         "correct_answer": 0
     },
     {
@@ -88,38 +91,39 @@ questions_veggies = [
 questions_gen_items = [
     {
         "question": "What is the Maori word for TV?",
-        "answers": ["Āporo", "Wharepaku", "Pākihi", "Kōtirotiro"],
-        "correct_answer": 0
-    },
-    {
-        "question": "What is the Maori word for COFFEE TABLE?",
-        "answers": ["Ārani", "Kōwhitiwhiti", "Karāti", "Tīhi"],
-        "correct_answer": 0
-    },
-    {
-        "question": "What is the Maori word for BED?",
-        "answers": ["Wharepēke", "Kōraka", "Pītiti", "Pūteketeke"],
-        "correct_answer": 2
-    },
-    {
-        "question": "What is the Maori word for DRAWERS?",
-        "answers": ["Kōremu", "Rēmana", "Wharepure", "Pūtangitangi"],
+        "answers": ["Pātakaarata", "Pouaka Whakaata", 
+                    "Whakamāharahara", "Hītoriwhakaāhua"],
         "correct_answer": 1
     },
     {
-        "question": "What is the Maori word for SMARTPHONE?",
-        "answers": ["Pūmate", "Wharemērēti", "Kōmerenika", "Merengi"],
+        "question": "What is the Maori word for COFFEE TABLE?",
+        "answers": ["Rākaukawhe", "Papara Kawhe", "Kōrerorangi", "Whakararaenga"],
+        "correct_answer": 1
+    },
+    {
+        "question": "What is the Maori word for BED?",
+        "answers": ["Whakararoturuma", "Moemoeāroha", "Whakapōtiki", "Moenga"],
         "correct_answer": 3
     },
     {
-        "question": "What is the Maori word for TOASTER?",
-        "answers": ["Wharepēke", "Kōraka", "Paramu", "Pūteketeke"],
+        "question": "What is the Maori word for DRAWERS?",
+        "answers": ["Whakararanga", "Pūtātara", "Toroa", "Whakapātaka"],
         "correct_answer": 2
     },
     {
-        "question": "What is the Maori word for OVEN?",
-        "answers": ["Kerepe", "Kōraka", "Pītiti", "Pūteketeke"],
+        "question": "What is the Maori word for SMARTPHONE?",
+        "answers": ["Waea Atamai", "Tamahiko", "Pātikaranga", "Kōreromahi"],
         "correct_answer": 0
+    },
+    {
+        "question": "What is the Maori word for TOASTER?",
+        "answers": ["Wharepēke", "Whakatōhi", "Paramu", "Pūteketeke"],
+        "correct_answer": 1
+    },
+    {
+        "question": "What is the Maori word for OVEN?",
+        "answers": ["Kerepe", "Kōraka", "Pītiti", "Umu"],
+        "correct_answer": 3
     },
 ]
 
@@ -127,36 +131,17 @@ random.shuffle(questions_fruit)
 random.shuffle(questions_veggies)
 random.shuffle(questions_gen_items)
 
+hard_mode_time_limit = 60  # 2 minutes
+
+
 def delete_multiple_lines():
     """Clear the console output."""
-    if os.name == "nt":  
+    if os.name == "nt":
         os.system("cls")
-    else:  
-        return 0
-
-def play_quiz(questions):
-    score = 0
-    total_questions = len(questions)
-
-    for question in questions:
-        score += ask_question(question)
-
-    print(f"Quiz finished! Your score: {score}/{total_questions}")
-
-    while True:
-        play_again = input("Would you like to play again? (Yes/No): ").lower()
-        if play_again == "yes":
-            delete_multiple_lines()
-            return True
-
-        elif play_again == "no":
-            delete_multiple_lines()
-            return False
-        else:
-            print("Hmm, that doesn't seem right. Only type in YES or NO.")
 
 
-def countdown(c):
+def countdown(c, time_limit):
+    """Countdown timer when starting quiz."""
     while c >= 0:
         if c > 0:
             print(f"{c}...", end="\r")
@@ -166,7 +151,39 @@ def countdown(c):
         c -= 1
 
 
+def play_quiz(questions, hard_mode=False):
+    """Play the quiz."""
+    score = 0
+    total_questions = len(questions)
+    time_limit = hard_mode_time_limit if hard_mode else 0
+    start_time = time.time()
+
+    for question in questions:
+        score += ask_question(question)
+
+        if time_limit > 0 and time.time() - start_time >= time_limit:
+            delete_multiple_lines()
+            print("Times Up!")
+            break
+
+    if time_limit == 0 or time.time() - start_time < time_limit:
+        print("Quiz finished! Your score:", score, "/", total_questions)
+
+    while True:
+        play_again = input("Would you like to play again? (Yes/No): ").lower()
+        if play_again == "yes":
+            delete_multiple_lines()
+            return True
+        elif play_again == "no":
+            delete_multiple_lines()
+            return False
+        else:
+            print("Hmm, that doesn't seem right. Only type in YES or NO.")
+
+
+
 def quiz_start():
+    """Start the quiz."""
     while True:
         print("Kia ora! Welcome to the Māori Language Quiz.")
         print("Please choose a category to test your Te Reo Māori knowledge:")
@@ -178,18 +195,33 @@ def quiz_start():
 
         if category_choice == "1":
             delete_multiple_lines()
-            countdown(3)
-            play_again = play_quiz(questions_fruit) 
+            difficulty = input("Choose difficulty level \n 1. Easy (no time limit)\n 2. Hard (1 minute time limit)\n\nSelect what difficulty you want: ").lower()
+            delete_multiple_lines()
+            countdown(3, hard_mode_time_limit if difficulty == "2" else 0)
+            if difficulty == "2":
+                play_again = play_quiz(questions_fruit, hard_mode=True)
+            else:
+                play_again = play_quiz(questions_fruit)
         elif category_choice == "2":
             delete_multiple_lines()
-            countdown(3)
-            play_again = play_quiz(questions_veggies)
+            difficulty = input("Choose difficulty level \n 1. Easy (no time limit)\n 2. Hard (1 minute time limit)\n\nSelect what difficulty you want: ").lower()
+            delete_multiple_lines()
+            countdown(3, hard_mode_time_limit if difficulty == "2" else 0)
+            if difficulty == "2":
+                play_again = play_quiz(questions_veggies, hard_mode=True)
+            else:
+                play_again = play_quiz(questions_veggies)
         elif category_choice == "3":
             delete_multiple_lines()
-            countdown(3)
-            play_again = play_quiz(questions_gen_items)
+            difficulty = input("Choose difficulty level \n 1. Easy (no time limit)\n 2. Hard (1 minute time limit)\n\nSelect what difficulty you want: ").lower()
+            delete_multiple_lines()
+            countdown(3, hard_mode_time_limit if difficulty == "2" else 0)
+            if difficulty == "2":
+                play_again = play_quiz(questions_gen_items, hard_mode=True)
+            else:
+                play_again = play_quiz(questions_gen_items)
         else:
-            print("That dosen't seem right. Please input a number between 1-3.")
+            print("That doesn't seem right.")
             continue
 
         if not play_again:
@@ -197,8 +229,9 @@ def quiz_start():
 
     print("Thank you for playing the Māori Language Quiz. Ka kite anō!")
 
+
 def ask_question(question):
-    """ Asks the questions """
+    """Asks the questions."""
     print(question["question"])
     for i, answer in enumerate(question["answers"]):
         print(f"{i+1}. {answer}")
@@ -208,7 +241,7 @@ def ask_question(question):
     while True:
         user_answer = input("Enter the number of your answer: ")
         if user_answer in valid_answers:
-            break 
+            break
         else:
             print("Sorry, that's not a valid option. Please choose an answer from 1 to 4.")
 
